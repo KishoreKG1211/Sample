e49b3bf950bb20d9260226f23d83cc0aad2ab747
Kishore Kumar Guna Sekar <kishorekumar.gunasekar@bounteous.com>
2025-09-03 16:45:50 +0530

    yml file updated

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: "mochawesome",
-  reporterOptions: {
-    reportDir: "cypress/results",
-    overwrite: false,
-    html: false,
-    json: true
-  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners if needed
+      // add plugins if needed
     },
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/results",
+      overwrite: false,
+      html: false,
+      json: true
+    }
   },
 });
