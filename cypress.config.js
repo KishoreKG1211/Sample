11b9daf24a3f06d6dde01249e0db9a9a653f1b80
Kishore Kumar Guna Sekar <kishorekumar.gunasekar@bounteous.com>
2025-09-03 15:38:10 +0530

    updated mocha report

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/results",
+    overwrite: false,
+    html: false,
+    json: true
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implement node event listeners if needed
     },
   },
 });
